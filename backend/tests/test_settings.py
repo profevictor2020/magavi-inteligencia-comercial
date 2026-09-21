437165eb9304f1782fe2eb810441eac331b8e7f6
Víctor Marín Almonacid <70583286+profevictor2020@users.noreply.github.com>
2026-09-21 18:04:29 -0300

    test: verificar arranque WSGI de producción

--- backend/tests/test_settings.py
+++ backend/tests/test_settings.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 
+from config.headers import add_whitenoise_headers
+
 
 def test_whitenoise_headers_configuration_is_callable():
     assert callable(settings.WHITENOISE_ADD_HEADERS_FUNCTION)
+    assert settings.WHITENOISE_ADD_HEADERS_FUNCTION is add_whitenoise_headers
